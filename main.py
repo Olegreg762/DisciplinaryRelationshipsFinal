706f59640937610a2053703649e38c348e14abab
Olegreg762 <109924953+Olegreg762@users.noreply.github.com>
2026-06-10 11:50:50 -0500

    Add presetner notes support for slideshow content

--- main.py
+++ main.py
@@ -18,7 +18,7 @@ def prev_slide():
 slide_content ={
     # 0 will be reserved for the rationale pdf and the final slide will be reserved for the reflection pdf
     0: {"img": "placeholder.pdf", "audio": "placeholder.mp3"},
-    1: {"img": "placeholder.png", "audio": "placeholder.mp3"},
+    1: {"img": "placeholder.png", "audio": "placeholder.mp3", "notes": "placeholder.txt"},
 }
 
 col1, col2 = st.columns(2)
@@ -31,8 +31,10 @@ with col1:
 with col2:
     if st.session_state.slide == 0:
         st.button("Start Slideshow", on_click=next_slide)
+
     elif st.session_state.slide > 0 and st.session_state.slide < len(slide_content) - 1:
         st.button("Next", on_click=next_slide)
+        
     elif st.session_state.slide == len(slide_content) - 1:
         st.button("Restart", on_click=next_slide)
 
@@ -40,9 +42,17 @@ with col2:
 if st.session_state.slide == 0:
     st.header("Rationale")
     st.pdf(slide_content[st.session_state.slide]["img"], height=500)
+
 elif st.session_state.slide > 0 and st.session_state.slide < len(slide_content) - 1:
+    
     st.image(slide_content[st.session_state.slide]["img"], caption=f"Slide {st.session_state.slide + 1}")
+
+    with open(slide_content[st.session_state.slide]["notes"], "r", encoding="utf-8") as file:
+        file_contents = file.read()
+    st.write(file_contents)
+
     st.audio(slide_content[st.session_state.slide]["audio"], autoplay=True)
+
 elif st.session_state.slide == len(slide_content) - 1:
     st.header("End of Slideshow")
     st.subheader("Reflection")
